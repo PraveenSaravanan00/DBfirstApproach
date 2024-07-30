a06e87524fa1eecc978f21b559f262227a77bf41
praveen <praveen.s@mitrahsoft.com>
2024-07-30 16:44:06 +0530

    done

---
+++ practiceForDbFirstApp/Models/Developer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practiceForDbFirstApp.Models;
+
+public partial class Developer
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Mobile { get; set; }
+}
---
+++ practiceForDbFirstApp/Models/PraveenContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace practiceForDbFirstApp.Models;
+
+public partial class PraveenContext : DbContext
+{
+    public PraveenContext()
+    {
+    }
+
+    public PraveenContext(DbContextOptions<PraveenContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Developer> Developers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=DefaultConnectionString");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Developer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Develope__3214EC079DCC7830");
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Mobile)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
